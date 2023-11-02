1dc4833827f57b8e9995842946b2c187a4a88447
Seyram Gabriel <seyramgabriel@gmail.com>
2023-11-02 00:36:05 -0700

    terraform project

--- EC2/local.tf
+++ EC2/local.tf
@@ -1,4 +1,4 @@
 locals {
   #ssm_path_prefix = "/seyram/us-east-2/vpc"
-  ssm_path_prefix = format("/%s/%s/%s", "seyram", "us-east-2", "vpc")
+  ssm_path_prefix = format("/%s/%s/%s", "kofisey", "us-east-2", "vpc")
 }
